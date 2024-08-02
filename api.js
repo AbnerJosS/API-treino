dc485d983674b3853d46d8eef790744e0993bd82
abner <abnerjose512@gmail.com>
2024-08-02 15:17:49 -0300

    ok

---
+++ api.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const servidor = express()
+
+servidor.get('/servidor/somar/:n1/:n2', (req, resp) =>{
+    let valor =Number(req.params.n1)
+    let valr =Number(req.params.n2)
+
+    let resultado= valor+valr
+
+    resp.send({
+        mensagem:resultado
+    })
+})
+servidor.listen(5050, () => console.log('a API deu certo'));
