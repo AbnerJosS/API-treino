f6476206e8f5a9afb323c2edb263d96f7fda4450
bruhsvicente <brutronica931@gmail.com>
2024-08-02 15:28:47 -0300

    Arquivo de multiplicação

--- api.js
+++ api.js
@@ -12,4 +12,12 @@ servidor.get('/servidor/somar/:n1/:n2', (req, resp) =>{
         mensagem:resultado
     })
 })
+
+servidor.get('/calculadora/multiplicar/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+    let m = n1 * n2
+    resp.send(`O resultado é ` + m)
+})
+
 servidor.listen(5050, () => console.log('a API deu certo'));
