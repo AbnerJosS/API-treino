70df9228d7545edc7fd3702bf624793b569922f1
bruhsvicente <brutronica931@gmail.com>
2024-08-02 16:15:14 -0300

    ...

--- api.js
+++ api.js
@@ -1,11 +1,10 @@
-import express from 'express'
-
-const servidor = express()
+import express from 'express';
+const servidor = express();
 
 servidor.get('/servidor/somar/:n1/:n2', (req, resp) =>{
     let valor =Number(req.params.n1)
     let valr =Number(req.params.n2)
-
+    
     let resultado= valor+valr
 
     resp.send({
